10de6e68f0c7767db605564e5a69dc10ae82f5a0
unicsys <unicornsystem4@gmail.com>
2025-07-16 00:15:08 -0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,8 @@
+# JDBC settings for PostgreSQL
+jdbc.driver=org.postgresql.Driver
+jdbc.url=jdbc:postgresql://localhost:5432/company_db
+jdbc.user=postgres
+jdbc.password=postgres
 
+# Ontop mapping settings
+ontop.mapping.format=obda
