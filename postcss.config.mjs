e9eef950febccb3320e7bb411fc08d11b5b56c28
unicsys <unicornsystem4@gmail.com>
2025-07-16 06:48:34 -0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,34 @@
-bin\ontop.bat endpoint --ontology=company.ttl --mapping=company.obda --properties=company.properties
+-- All data from uni1
+SELECT 'STUDENTS' as table_name, '---' as divider;
+SELECT * FROM uni1.student;
+
+SELECT '' as blank;
+SELECT 'ACADEMICS' as table_name, '---' as divider;
+SELECT * FROM uni1.academic;
+
+SELECT '' as blank;
+SELECT 'COURSES' as table_name, '---' as divider;
+SELECT * FROM uni1.course;
+
+SELECT '' as blank;
+SELECT 'TEACHING' as table_name, '---' as divider;
+SELECT * FROM uni1.teaching;
+
+SELECT '' as blank;
+SELECT 'REGISTRATIONS' as table_name, '---' as divider;
+SELECT * FROM uni1."course-registration";
+
+
+==============
+
+-- All data from uni2
+SELECT 'PEOPLE' as table_name, '---' as divider;
+SELECT * FROM uni2.person;
+
+SELECT '' as blank;
+SELECT 'COURSES' as table_name, '---' as divider;
+SELECT * FROM uni2.course;
+
+SELECT '' as blank;
+SELECT 'REGISTRATIONS' as table_name, '---' as divider;
+SELECT * FROM uni2.registration;
