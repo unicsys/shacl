efabe4185aeadbc61a349514999720f6eded5b23
unicsys <unicornsystem4@gmail.com>
2025-10-07 23:23:17 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,1 @@
-ShortForm,FullForm
-TEC,Turbine Exhaust Case
-JCN,Job Contol Number
-ESN,Engine Serial Number
-CEMS,Comprehensive Engine Management System
-EHM,Engine Health Management
-FMECA,"Failure Mode, Effects, and Criticality Analysis"
-UBL,Usage-Based Lifing
-SRAN,Stock Record Account Number
-LCN,Logistics Control Number
 
