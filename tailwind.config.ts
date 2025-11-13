8f6bf77bd3890f3f94b5cf95306c2bad28dd2fdd
unicsys <unicornsystem4@gmail.com>
2025-11-13 07:50:41 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,153 @@
+@prefix bfo: <http://purl.obolibrary.org/obo/> .
+@prefix iof-core: <https://spec.industrialontologies.org/ontology/core/Core/> .
+@prefix ockms: <https://ontologyhub.rtx.com/RTXEnterpriseOntologies/OCKMSDigitalThreadOntology/> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+
+<https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssemblyIdentifier/DCLP22> a ockms:CircuitCardAssemblyIdentifier ;
+    ockms:hasTextValue "DCLP22" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssembly/752_6502_003_B_DCLP22> .
+
+<https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssemblyIdentifier/F0TCR9> a ockms:CircuitCardAssemblyIdentifier ;
+    ockms:hasTextValue "F0TCR9" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssembly/752_6502_003_B_F0TCR9> .
+
+<https://ontologyhub.rtx.com/ockms-instances/DefectClassificationDescription/C7> a ockms:DefectClassificationDescription ;
+    rdfs:label "Dead component" ;
+    iof-core:describes <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_R143_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown1_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown2_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown3_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown4_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown5_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown6_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown7_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown8_C7>,
+        <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown9_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/DefectClassificationDescription/W130> a ockms:DefectClassificationDescription ;
+    rdfs:label "Damage During Testing" ;
+    iof-core:describes <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_F0TCR9_R152_W130> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesignIdentifier/705_2437_180> a ockms:ElectronicComponentDesignIdentifier ;
+    rdfs:label "Component P/N 705-2437-180" ;
+    ockms:hasTextValue "705-2437-180" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesign/705_2437_180> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesignIdentifier/705_2437_414> a ockms:ElectronicComponentDesignIdentifier ;
+    rdfs:label "Component P/N 705-2437-414" ;
+    ockms:hasTextValue "705-2437-414" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesign/705_2437_414> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ReferenceDesignator/752_6502_003_R143> a ockms:ReferenceDesignator ;
+    ockms:hasTextValue "R143" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R143> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ReferenceDesignator/752_6502_003_R152> a ockms:ReferenceDesignator ;
+    ockms:hasTextValue "R152" ;
+    iof-core:designates <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R152> .
+
+<https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssembly/752_6502_003_B_DCLP22> a ockms:CircuitCardAssembly ;
+    rdfs:label "CCA S/N DCLP22" ;
+    iof-core:hasComponentPartAtAllTimes <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_R143_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown1_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown2_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown3_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown4_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown5_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown6_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown7_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown8_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown9_705_2437_414> .
+
+<https://ontologyhub.rtx.com/ockms-instances/CircuitCardAssembly/752_6502_003_B_F0TCR9> a ockms:CircuitCardAssembly ;
+    rdfs:label "CCA S/N F0TCR9" ;
+    iof-core:hasComponentPartAtAllTimes <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_F0TCR9_R152_705_2437_180> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesign/705_2437_180> a ockms:ElectronicComponentDesign ;
+    rdfs:label "Component Design 705-2437-180" ;
+    iof-core:prescribes <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_F0TCR9_R152_705_2437_180> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentDesign/705_2437_414> a ockms:ElectronicComponentDesign ;
+    rdfs:label "Component Design 705-2437-414" ;
+    iof-core:prescribes <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_R143_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown1_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown2_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown3_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown4_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown5_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown6_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown7_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown8_705_2437_414>,
+        <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown9_705_2437_414> .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_R143_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown1_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown2_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown3_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown4_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown5_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown6_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown7_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown8_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown9_C7> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_F0TCR9_R152_W130> a ockms:Defect .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_R143_705_2437_414> a ockms:ElectronicComponent ;
+    bfo:BFO_0000171 <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R143> ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_R143_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown1_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 1)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown1_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown2_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 2)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown2_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown3_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 3)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown3_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown4_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 4)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown4_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown5_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 5)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown5_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown6_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 6)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown6_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown7_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 7)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown7_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown8_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 8)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown8_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_DCLP22_unknown9_705_2437_414> a ockms:ElectronicComponent ;
+    rdfs:label "Component 705-2437-414 on DCLP22 (Unknown Location 9)" ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_DCLP22_unknown9_C7> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponent/752_6502_003_F0TCR9_R152_705_2437_180> a ockms:ElectronicComponent ;
+    bfo:BFO_0000171 <https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R152> ;
+    bfo:BFO_0000196 <https://ontologyhub.rtx.com/ockms-instances/Defect/752_6502_003_F0TCR9_R152_W130> .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R143> a ockms:ElectronicComponentLocation .
+
+<https://ontologyhub.rtx.com/ockms-instances/ElectronicComponentLocation/752_6502_003_R152> a ockms:ElectronicComponentLocation .
+
 
